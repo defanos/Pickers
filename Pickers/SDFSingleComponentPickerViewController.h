0feb57b668cffc22cf10efb10e40770939f68233
Salvatore De Fano <salvatore.defano@yoox.com>
2015-06-10 10:05:45 +0200

    SingleComponentPicker

--- Pickers/SDFSingleComponentPickerViewController.h
+++ Pickers/SDFSingleComponentPickerViewController.h
@@ -10,4 +10,6 @@
 
 @interface SDFSingleComponentPickerViewController : UIViewController
 
+<UIPickerViewDelegate, UIPickerViewDataSource>
+
 @end
--- Pickers/SDFSingleComponentPickerViewController.m
+++ Pickers/SDFSingleComponentPickerViewController.m
@@ -10,13 +10,17 @@
 
 @interface SDFSingleComponentPickerViewController ()
 
+@property (weak, nonatomic) IBOutlet UIPickerView *singlePicker;
+@property (strong, nonatomic) NSArray *characterNames;
+
 @end
 
 @implementation SDFSingleComponentPickerViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.characterNames = @[@"Luke", @"Leia", @"Han", @"Chewbacca",@"Artoo", @"Threepio", @"Lando"];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +28,38 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)buttonPressed:(id)sender {
+    NSInteger row = [self.singlePicker selectedRowInComponent:0];
+    NSString *selected = self.characterNames[row];
+    NSString *title = [[NSString alloc]
+                       initWithFormat:@"You selected %@!", selected];
+    UIAlertView *alert = [
+                          [UIAlertView alloc]
+                          initWithTitle:title
+                          message:@"Thank you for choosing."
+                          delegate:nil
+                          cancelButtonTitle:@"You're Welcome"
+                          otherButtonTitles:nil
+                          ];
+    [alert show];
 }
-*/
+
+#pragma mark -
+#pragma mark Picker Data Source Methods
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return [self.characterNames count];
+}
+
+#pragma mark Picker Delegate Methods
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return self.characterNames[row];
+}
 
 @end
